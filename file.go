4046014d6d139f782ba61924512b7930b63288da
闫亚洲 <yanyazhou@cetccloud.com>
2021-02-10 14:15:46 +0800

    支持异步往文件里写日志

--- file.go
+++ file.go
@@ -9,6 +9,8 @@ import (
 
 // 往文件里输出日志相关代码
 
+var maxChanSize = 50000
+
 // FileLogger 结构体
 type FileLogger struct {
 	Level       LogLevel
@@ -17,6 +19,16 @@ type FileLogger struct {
 	fileObj     *os.File
 	errFileObj  *os.File
 	maxFileSize int64
+	logChan     chan *logMsg
+}
+
+type logMsg struct {
+	level     LogLevel
+	timeStamp string
+	fileName  string
+	funcName  string
+	msg       string
+	lineNo    int
 }
 
 // NewFileLogger 构造函数
@@ -31,6 +43,7 @@ func NewFileLogger(levelStr, filePath, fileName string, maxSize int64) *FileLogg
 		filePath:    filePath,
 		fileName:    filePath,
 		maxFileSize: maxSize,
+		logChan:     make(chan *logMsg, maxChanSize),
 	}
 	fl.initFile(filePath, fileName)
 	return fl
@@ -49,20 +62,56 @@ func (f *FileLogger) initFile(fp, fn string) {
 	}
 	f.fileObj = fileObj
 	f.errFileObj = errFileObj
+	// 开启一个后台goroutine用来异步写日志
+	go f.writeLogBackground()
 }
 
+// enable 判断需要打印哪些级别的日志
 func (f *FileLogger) enable(logLevel LogLevel) bool {
 	return logLevel >= f.Level
 }
 
+// writeLogBackground 从通道中取日志，然后后台往文件里写日志
+func (f *FileLogger) writeLogBackground() {
+	for {
+		select {
+		// 从通道中去log
+		case logTmp := <-f.logChan:
+			logInfo := fmt.Sprintf("%s [%s] %s:%s:%d %s\n", logTmp.timeStamp,
+				parseLogLevelStr(logTmp.level), logTmp.fileName, logTmp.funcName, logTmp.lineNo, logTmp.msg)
+			fmt.Fprintf(f.fileObj, logInfo)
+			if logTmp.level >= ERROR {
+				fmt.Fprintf(f.errFileObj, logInfo)
+			}
+		// 取不到就跳过，先休息500毫秒
+		default:
+			time.Sleep(time.Millisecond * 500)
+		}
+
+	}
+}
+
+// log 处理日志，将日志发送到通道中
 func (f *FileLogger) log(lv LogLevel, format string, a ...interface{}) {
 	if f.enable(lv) {
 		msg := fmt.Sprintf(format, a...)
 		now := time.Now()
 		funcName, fileName, lineNo := getInfo(3)
-		fmt.Fprintf(f.fileObj, "%s [%s] %s:%s:%d %s\n", now.Format("2006-01-02 15:04:05"), parseLogLevelStr(lv), fileName, funcName, lineNo, msg)
-		if lv >= ERROR {
-			fmt.Fprintf(f.errFileObj, "%s [%s] %s:%s:%d %s\n", now.Format("2006-01-02 15:04:05"), parseLogLevelStr(lv), fileName, funcName, lineNo, msg)
+		// 先把日志发送到通道中
+		// 造一个logMsg对象
+		logTmp := &logMsg{
+			level:     lv,
+			timeStamp: now.Format("2006-01-02 15:04:05"),
+			fileName:  fileName,
+			funcName:  funcName,
+			msg:       msg,
+			lineNo:    lineNo,
+		}
+		select {
+		// 将log放入通道中
+		case f.logChan <- logTmp:
+		// 把日志丢掉保证不出现阻塞
+		default:
 		}
 	}
 }
